bd2f9869bb67d1dfb1b17e0060f511bda2382aba
mohamed-bouzerouata <mohameddevelopper3@gmail.com>
2020-05-31 21:11:19 +0100

    remake the app

--- FrontEnd/pages/_app.js
+++ FrontEnd/pages/_app.js
@@ -1,19 +1,14 @@
 import App  from 'next/app'
 import Page from '../components/Page'
-import ApolloClient from 'apollo-boost'
-import { ApolloProvider } from 'apollo/react-hooks'
 
-const client = new ApolloClient({
-    uri : 'localhost:4000'
-})
+
 
 function MyApp({ Component, pageProps }) {
     return (
-        <ApolloProvider client={client}>
             <Page>
                 <Component />
             </Page>
-        </ApolloProvider>
+    
     
     )
 }
--- FrontEnd/pages/sell.js
+++ FrontEnd/pages/sell.js
@@ -4,7 +4,6 @@ import Link from 'next/link'
 const sell = () => {
     return (
         <div>
-            <p>Hello from sell page</p>
         <Link href='/'>
             <a>Home</a>
         </Link>
