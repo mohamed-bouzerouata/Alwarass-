a087e1e0accae4b30be54ca948dcb2fa9689a8e3
mohamed-bouzerouata <mohameddevelopper3@gmail.com>
2020-06-02 14:46:02 +0100

    react/graphQL

--- FrontEnd/pages/_app.js
+++ FrontEnd/pages/_app.js
@@ -1,13 +1,50 @@
 import App  from 'next/app'
 import Page from '../components/Page'
+import ApolloClient from 'apollo-boost';
+import { render } from 'react-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { useQuery} from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
 
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/',
+});
+
+const FETCH_dATA = gql`
+    query {
+        items {
+            id
+            title
+            description
+            image
+            largeImage
+            price
+        }
+    }
+`
+function FetchData() {
+    const { loading, error, data } = useQuery(FETCH_dATA);
+    if(loading) return <p>Loading...</p>;
+    if(error) return <p>Error</p>
+
+    return data.items.map(({ id, title }) =>  (
+        <div key={id}>
+            <p>
+                {id} : {title   }
+            </p>
+        </div>
+    ));
+} 
 
 
 function MyApp({ Component, pageProps }) {
     return (
-            <Page>
-                <Component />
-            </Page>
+            <ApolloProvider client={client}>
+                <Page>
+                    <Component />
+                </Page>
+            </ApolloProvider>
     
     
     )
